c75a35ec053b0fc2d97dd01e340aaa4a2289a989
Diljit VJ <diljitvj@gmail.com>
2019-07-12 17:27:23 +0530

    Add routes to app

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const routes = require("./lib/routes");
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use((req, res, next) => {
-  console.log("I am a global middleware");
+  // Add authentication middleware here
   next();
 });
 
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log("I am a route specific middleware");
-    next();
-  },
-  function(req, res) {
-    res.send("Hello World!");
-  }
-);
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use("/api", routes);
+app.listen(port, () => console.log(`Shop app listening on port ${port}!`));
