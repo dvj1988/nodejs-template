f1ac9ecc45ddcf5451233c0761a7017521c0b056
Diljit VJ <diljitvj@gmail.com>
2019-07-12 17:27:16 +0530

    Express: Middlewares

--- index.js
+++ index.js
@@ -5,32 +5,20 @@ const port = 3000;
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get("/", function(req, res) {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  console.log("I am a global middleware");
+  next();
 });
 
-app.post("/", function(req, res) {
-  res.send("Got a POST request");
-});
-
-app.put("/user", function(req, res) {
-  res.send("Got a PUT request at /user");
-});
-
-app.delete("/user", function(req, res) {
-  res.send("Got a DELETE request at /user");
-});
-
-// using path params
-app.get("/users/:userId/books/:bookId", function(req, res) {
-  //demo in postman
-  res.send(req.params);
-});
-
-// using query params
-app.get("/users", function(req, res) {
-  //demo in postman
-  res.send(req.query);
-});
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("I am a route specific middleware");
+    next();
+  },
+  function(req, res) {
+    res.send("Hello World!");
+  }
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
