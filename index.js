b93c480d7518d0c8a29d623594c3c067f053c569
Diljit VJ <diljitvj@gmail.com>
2019-07-15 18:31:48 +0530

    Complete routes for the users model

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const routes = require("./lib/routes");
+const { userRepository } = require("./lib/repositories");
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use((req, res, next) => {
   // Add authentication middleware here
+  res.locals.userRepository = userRepository;
   next();
 });
 
