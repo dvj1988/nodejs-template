14268fc518015f67a5f3b0b9e5bbe38c97d408af
Diljit VJ <diljitvj@gmail.com>
2019-07-18 15:32:16 +0530

    Order repository, routes and resolvers

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const port = 3000;
-const routes = require("./lib/routes");
-const { userRepository, itemRepository } = require("./lib/repositories");
+const routes = require('./lib/routes');
+const { userRepository, itemRepository, orderRepository } = require('./lib/repositories');
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -11,8 +12,10 @@ app.use((req, res, next) => {
   // Add authentication middleware here
   res.locals.userRepository = userRepository;
   res.locals.itemRepository = itemRepository;
+  res.locals.orderRepository = orderRepository;
   next();
 });
 
-app.use("/api", routes);
+app.use('/api', routes);
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Shop app listening on port ${port}!`));
